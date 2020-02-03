67f356f11312d1667a43bfb1c5d4990ac0da1183
kaminion <anysweetimes@naver.com>
2020-02-03 17:55:55 +0900

    webpack config

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const App = () =>{
+
+    return (
+        <h3 className="title">Hello, React</h3>
+    )
+
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	entry: "./src/test.js",
+	entry: "./src/index.js",
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname + "/build")
@@ -11,17 +12,27 @@ module.exports = {
 	module:
 	{
 		rules: [{
+			test: /\.(js|jsx)$/,
+			exclude: "/node_modules",
+			use: ['babel-loader'],
+		}, {
 			test: /\.html$/,
 			use: [{
 				loader: 'html-loader',
 				options: {minimize: true}
 			}]
+		},{
+			test: /\.css$/,
+			use: [MiniCssExtractPlugin.loader, 'css-loader']
 		}]
 	},
 	plugins: [
 		new HtmlWebPackPlugin({
 			template: './public/test.html',
 			filename: 'index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'style.css'
 		})
 	]
 };
